b9464b41897e492849f43631c47ed2e1828c057f
Maddisen Gumeson <trackstar800@gmail.com>
2016-04-26 07:45:15 -0600

    Scripts from a different game to show arrays

---
+++ GemSpawn copy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawn : MonoBehaviour {
+	public playerhealth PlayerHealth;
+	public GameObject gem;
+	public float spawnTime = 15f;
+	public Transform[] gempoints;
+	
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("GemSpawner", spawnTime, spawnTime);
+	}
+	
+	void GemSpawner ()
+	{
+		if(PlayerHealth.currentHealth <= 0f)
+		{
+			return;
+		}
+		int spawnPointIndex = Random.Range (0, gempoints.Length);
+		
+		Instantiate (gem, gempoints[spawnPointIndex].position, gempoints[spawnPointIndex].rotation);
+	}
+}
