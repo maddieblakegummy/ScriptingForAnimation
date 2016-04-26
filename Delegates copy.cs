b3bab6e08cc638d9d4f257b5311e50a1df39a985
Maddisen Gumeson <trackstar800@gmail.com>
2016-04-26 07:57:37 -0600

    A new script to show delegates

---
+++ Delegates copy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Delegates : MonoBehaviour {
+
+	delegate void ShowDelegates();
+	ShowDelegates completedDelegates;
+
+	void Start ()
+	{
+		completedDelegates += Proof;
+		completedDelegates += WrittenWords;
+
+		completedDelegates();
+	}
+
+	void Proof ()
+	{
+		print ("Here is a delegate");
+	}
+	void WrittenWords()
+	{
+		Debug.Log ("Here is a multidelegate");
+	}
+}
